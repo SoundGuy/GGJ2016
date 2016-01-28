bc622d770f4857aa1e70807fa638fb04d26b7473
Itay Niv <itayniv@gmail.com>
2016-01-28 22:58:01 +0200

    First experiment added

---
+++ Assets/Scripts/Experience_01.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Experience_01 : MonoBehaviour {
+
+	public InputField mainInputField;
+
+	// Use this for initialization
+	void Start () {
+
+		mainInputField.text = "Enter Text Here...";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("return")) {
+			Debug.Log(mainInputField.text);
+		}
+	}
+}
